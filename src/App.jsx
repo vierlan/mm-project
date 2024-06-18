6f172bd7c6033e6376fe75cfe524da4c0bcefe04
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-06-18 18:00:18 +0100

    initial commit with bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Signin from './components/Signin'
+
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Signin />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <>
+      <div className='sign-in-container'>
+        <button className="buttonGoogle" >Sign in with Google</button>
+        <button className='buttonSignin'>Signin with e-mail</button>
+      </div>
+    </>
+  )
+}
+
+export default Signin
